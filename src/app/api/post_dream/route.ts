0302705a0ef4d8369e9483acecfc769839f237a7
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-15 15:56:07 +0100

    testing iå adress

---
+++ src/app/api/post_dream/route.ts
@@ -0,0 +1,30 @@
+// route.ts
+import type { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient } from "mongodb";
+
+type ReqData = {
+  mail: string;
+  name: string;
+  blob: string;
+};
+const client = new MongoClient(process.env.MONGO_DB_URL as string);
+export const POST = async (req: any, res: NextApiResponse<ReqData>) => {
+  const data = await req.json();
+
+  console.log(` documents inserted`, data);
+  try {
+    await client.connect();
+    const database = await client.db("dream_catcher");
+    const collection = database.collection("dreams");
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    const result = await collection.insertOne(data);
+    return Response.json(result);
+  } catch (err) {
+    console.log(err);
+    return { err: "buhuuuu" };
+  }
+};
--- src/features/VoiceRecorder/components/VoiceRecorder.component.tsx
+++ src/features/VoiceRecorder/components/VoiceRecorder.component.tsx
@@ -11,6 +11,8 @@ import { InfoText } from "@/components/InfoText";
 export const VoiceRecorder: FC = () => {
   const { transcript, listening, resetTranscript } = useSpeechRecognition();
   const [startSharing, setStartSharing] = useState(false);
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
   const [recording, setRecording] = useState(false);
   const startRecording = () => {
     setRecording(true);
@@ -36,10 +38,40 @@ export const VoiceRecorder: FC = () => {
     if (!recordingBlob) return;
     console.log("Recording Blob:", recordingBlob);
   }, [recordingBlob]);
+
+  const postDream = async () => {
+    try {
+      fetch("/api/post_dream", {
+        method: "POST",
+        body: JSON.stringify({ name: name, mail: mail, blob: "blob" }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
     <>
       {startSharing ? (
-        <form className='items-center flex flex-col gap-5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-3/4'>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            postDream();
+          }}
+          className='items-center flex flex-col gap-5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-3/4'
+        >
           <h2 className=' text-center text-1xl'>
             <span className='font-bold'>What are you</span> Dreaming about
           </h2>
@@ -50,22 +82,19 @@ export const VoiceRecorder: FC = () => {
           />
           {transcript}
           <input
+            onChange={(e) => setName(e.target.value)}
             required
             placeholder='Name'
             className=' placeholder:text-black rounded-md border p-5 bg-transparent'
           />
           <input
+            onChange={(e) => setMail(e.target.value)}
             required
             placeholder='Mail'
             className=' placeholder:text-black rounded-md border p-5 bg-transparent '
           />
           {recording && <h2>RECORDING</h2>}
-          <button
-            onClick={() => {
-              setRecording(false);
-            }}
-            className=' hover:bg-blue-100 border border-white p-10 rounded-md shadow-md bg-transparent'
-          >
+          <button className=' hover:bg-blue-100 border border-white p-10 rounded-md shadow-md bg-transparent'>
             SAVE RECORDING
           </button>{" "}
         </form>

f6ca76b8adf626bf9f3795f81960053d3092adb7
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2024-01-17 20:18:49 +0100

    testing

--- src/features/Dreams/components/DisplayDreams.component.tsx
+++ src/features/Dreams/components/DisplayDreams.component.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Spinner } from "@/ui";
 import React, { useEffect, useState } from "react";
 interface Dream {
   name: string;
@@ -7,23 +8,31 @@ interface Dream {
 }
 export const DisplayDreams = () => {
   const [dreams, setDreams] = useState<Dream[] | null>(null);
+  const [loading, setLoading] = useState(false);
   const fetchDreams = async () => {
+    setLoading(true);
     const data = fetch("/api/dream", {
       method: "GET",
     })
       .then((res) => res.json())
       .then((res) => {
-        setDreams(res);
         console.log(res);
+        setLoading(false);
+        setDreams(res);
       })
       .catch((err) => {
         console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   };
   useEffect(() => {
     fetchDreams();
   }, []);
-  console.log(dreams, "stststet");
+  if (loading) return <Spinner />;
+  if (!dreams) return <h2>Could Not Get Dreams</h2>;
+
   return (
     <div className=' mt-20 flex flex-col items-center'>
       {dreams?.map((dream, i) => {
@@ -31,7 +40,7 @@ export const DisplayDreams = () => {
           <div key={i}>
             <p>Name: {dream.name}</p>
             <p>Mail: {dream.mail}</p>
-            <audio controls src={`data:audio/mpeg;base64,${dream.blob}`} />
+            <audio controls src={`data:audio/mpeg;base64,${dream?.blob}`} />
           </div>
         );
       })}
